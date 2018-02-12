bfddcfd0da55946d600e6f6ef3c5e96a050aedbf
Štefan Polyak <stefan@flimmit.com>
2018-02-12 19:10:27 +0100

    if regex

--- components/content.js
+++ components/content.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 export function ContentSamsung(props) {
+	// idee in props send vender (lg or samsung or ....)
 	return (
 		<ul>
 			<h1>Samsung</h1>
 
-			<li>Technology: 		{props.values[0]} </li>
-			<li>Location: 			{props.values[1]} </li>
-			<li>Size in inches: 	{props.values[2]} </li>
-			<li>Model year: 		{props.values[3]} </li>
-			<li>Serie: 				{props.values[4]} </li>
-			<li>Feature: 			{props.values[5]} </li>
-			<li>Manufacturing info: {props.values[6]} </li>
+			<li>Technology:		<span>{props.values[0]}</span> </li>
+			<li>Location:		<span>{props.values[1]}</span> </li>
+			<li>Size in inches: <span>	{props.values[2]}</span> </li>
+			<li>Model year: 	<span>	{props.values[3]}</span> </li>
+			<li>Serie: 			<span>	{props.values[4]}</span> </li>
+			<li>Feature: 		<span>	{props.values[5]}</span> </li>
+			<li>Manufacturing info: <span>{props.values[6]}</span> </li>
 		</ul>
 	);
 }
@@ -44,4 +45,78 @@ export var samsungTechnologyArray = {
 				'M':'2017 HD',
 				'MU':'2017 UHD',
 				'Q':'2017 QLED'
-			};
+			},
+			oldSamsungTechnology = {
+				'LN':'LCD',
+				'FP':'Plasma',
+				'HP':'Plasma',
+				'HL':'DLP',
+				'TX':'CRT'
+			},
+			oldSamsungModel = {
+				'T':'2007',
+				'S':'2006',
+				'R':'2005',
+				'P':'2004',
+				'N':'2003',
+				'M':'2002'
+			},
+			sonyModel = {
+				'R':'Primary',
+				'W':'Middle',
+				'S':'Premium',
+				'X':'Premium'
+			},
+			sonyYear = {
+				'A':'2013',
+				'B':'2014',
+				'C':'2015',
+				'D':'2016',
+				'E':'2017',
+				'F':'2018'
+			}, 
+			panasonicYear = {
+				'A':'2014',
+				'C':'2015',
+				'D':'2016',
+				'E':'2017'
+			},
+			panasonicResolutions = {
+				'S':'Full HD',
+				'X':'UHD',
+				'R':'Curved HD',
+				'Z':'OLED'
+			}, panasonicCountry = {
+				'B':'United Kingdom or Brazil',
+				'U':'USA',
+				'E':'Spain, Poland',
+				'A':'Australia',
+				'N':'France, Portugal, Canada'
+			}, lgNonOledType = {
+				'S':'Super UHD, Nano Cell Matrix',
+				'U':'UHD LED',
+				'E':'OLED until 2016',
+				'L':'LED',
+				'P':'Plasma up to 2012'
+			}, lgNonOledYear = {
+				'J':'2017',
+				'H':'2016',
+				'F':'2015',
+				'G':'2015',
+				'B':'2014',
+				'C':'2014',
+				'N':'2013',
+				'A':'2013',
+				'W':'2012',
+				'M':'2012',
+				'S':'2012',
+				'V':'2017'
+			}, lgNonOledTuner = {
+				'T':'DVB-T',
+				'C':'DVB-C',
+				'S':'DVB-S2/T/C',
+				'V':'DVB-S2/T2/C',
+				'U':'HD',
+				'B':'HD',
+				'0':'DVB-T/C'
+			}
--- components/input.js
+++ components/input.js
@@ -7,30 +7,50 @@ class Input extends Component {
 	constructor(props) {
 		super(props);
 		
-		this.handleChangeInSelect = this.handleChangeInSelect.bind(this);
 		this.state = { input: '', value: '', typ: '' };
 	}
 
-	handleChangeInSelect(event) {
-		this.setState({ typ: event.target.value });
-	}
-
 	render() {
+		/**
+		 * [REGEX]
+		 * @type {RegExp}
+		 */
+		const samsung = /^([QUPLHK]{1})([NEA]{1})([0-9]{2})([A-FQMLKHFUS]{1,2})([0-9]{4})([A-Z]{1})([A-Z]{3})$/;
+		const samsungOld = /^([LNFPHTX]{2})?([-]{1})?([TSRPN]{1})([0-9]{2})([0-9]{2})([A-Z]{1})([A-Z]{1})([/A-Z0-9]{3})$/;
+		const lgNonOled = /^([0-9]{2})([SUELP]{1})([JHFGBCNAWMSV]{1})([0-9]{1})([0-9]{1})([0-9]{1})([TCUSVB0]{1})$/;
+		const lgOled = /^([OLED]{4})([0-9]{2})([A-Z]{1})([0-9]{1})([TCSVUBt0]{1})$/;
+		const sonyNew = /^([KD]){2}?([L]){1}?([-]{1})?([0-9]{2})([RWSX]{1})([ABCDEFG]{1})([0-9]{1})([0-9]{1})([0-9]{2})([A-Z]{4})?$/;
+		const sonyOld = /^([0-9]{2})([RWSX]{1})([0-9]{1})([0-9]{1})([0-9]{2})([A-Z]{1})$/;
+		const panasonic = /^([TX]{2})?([-]{1})?([0-9]{2})([ABCDEFG]{1})([SXRZW]{1})([0-9]{1})([0-9]{2})([A-Z0-9]{1})$/;
+		
 		const isInputCorrect = this.state.input;
-		const samsungRegex = /^[QUPLHKquplhk]{1}[NEAnea]{1}[0-9]{2}[A-FQMLKHFa-fqmlkhf]{1}([USus]{1})?[0-9]{4}[a-zA-Z]{1}[a-zA-Z]{3}$/g;
-		const lgNonOled = /^[0-9]{2}[SUELPsuelp]{1}[JHFGBCNAWMSVjhfgbcnawmsv]{1}[0-9]{1}[0-9]{1}[0-9]{1}[TCUSVBtcusvb0]{1}$/g;
-		let regex = '';
-		if (this.state.typ === 'lg') {
-			regex = samsungRegex;
-		} else if (this.state.typ === 'samsung') {
-			regex = samsungRegex;
-		} else if (this.state.typ === 'panasonic') {
-			regex = samsungRegex;
-		} else {
-			regex = samsungRegex;
-		}
-		const testInput = regex.test(isInputCorrect);
+		var input = isInputCorrect.toUpperCase();
+		var regex = [samsung, samsungOld, lgOled, lgNonOled, sonyOld, sonyNew, panasonic];
+		var testInput = false;
 
+		if (input) {
+			for (var i = 0; i <= regex.length - 1; i++) {
+				let match = input.match(regex[i]);
+
+				if (match) { // add samsungOld
+					if (match[0].length == '14' || match[0].length == '13' ) {
+						regex = samsung;
+					} else if (match[0].length == '9') {
+						regex = lgOled;
+					} else if (match[0].length == '8' && isNaN(match[3]) ) {
+						regex = lgNonOled;
+					} else if (match[0].length == '11' && match[0] == "KD") {
+						regex = sonyNew;
+					} else if (match[0].length == '8' && !isNaN(match[3]) ) {
+						regex = sonyOld;
+					} else if (match[0].length == '11' && match[0] == "TX") {
+						regex = panasonic;
+					}
+					testInput = regex.test(input);
+				}
+			}
+		}
+		
 		function PutData(props) {
 			const input = isInputCorrect.toUpperCase();
 			const arr = Array.from(input);
@@ -55,26 +75,27 @@ class Input extends Component {
 			if (testInput) { 
 				return <PutData data={props} />;
 			} else { 
-				return <p>Please insert TV Model Number to input.</p>; 
+				return (
+					<div>
+						<p>Please insert TV Model Number to input.</p>
+						<p>Samsung = UE55MU6400UXZG</p>
+						<p>Samsung old = LN-T4681FX/XA</p>
+						<p>LG OLED = OLED55C7V</p>
+						<p>LG = 43LF540V</p>
+						<p>Sony = KD-55X8507C</p>
+						<p>Sony old = 65S8005C</p>
+						<p>Panasonic = TX-58DX902B</p>
+					</div>
+				); 
 			}
 		}
 
 		return (
 			<div>
-				<p>
-					<select	onChange={this.handleChangeInSelect} >
-						<option value=""></option>
-						<option value="samsung">Samsung</option>
-						<option value="lg">LG</option>
-						<option value="sony">Sony</option>
-						<option value="panasonic">Panasonic</option>
-					</select>
-				</p>
-
 				<input
 					type="text"
-					value={this.state.input}
 					onChange={event => this.setState({ input: event.target.value })}
+					value={this.state.input}
 				/>
 
 				<GetData data={this.state.typ} isInputCorrect={false} />
